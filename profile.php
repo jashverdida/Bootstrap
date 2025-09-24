4b70db058a4b07a6e2e1c9bcd86a0a47aacad824
Jash <JashmineVerdida08@gmail.com>
2025-09-24 18:44:00 +0800

    Show Back to Catalog for all logged-in users, Add to Kit only for non-guest heroes

--- profile.php
+++ profile.php
@@ -500,6 +500,8 @@ if (isset($_GET['clear_registration'])) {
                                                     <input type="hidden" name="product_id" value="<?php echo $productId; ?>">
                                                     <button type="submit" name="add_to_cart" class="btn btn-spidey w-100 mb-2">Add to Kit</button>
                                                 </form>
+                                            <?php endif; ?>
+                                            <?php if (isset($_SESSION['user'])): ?>
                                                 <a href="profile.php?user=<?php echo isset($user['id']) ? $user['id'] : '' ?>" class="btn btn-outline-spidey w-100">Back to Catalog</a>
                                             <?php endif; ?>
                                         </div>
